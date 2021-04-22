eead28a188c114e20ce4ac013d5773c410dd78d4
Mohammed <atefhamosa@gmail.com>
2021-04-22 06:50:59 +0200

    Update Button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,12 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Button = (props) => {
-  return <button>{props.name}</button>;
+  const { name } = props;
+  return <button type="button"> {name} </button>;
 };
 
 // Props types and defaults
 Button.propTypes = {
   name: PropTypes.string,
+};
+Button.defaultProps = {
+  name: '0',
 };
 
 export default Button;
