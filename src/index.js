506037109144be9d99195d79dc1a6e4f81b14ee1
Mohammed <atefhamosa@gmail.com>
2021-04-22 04:56:05 +0200

    Add basic styles

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import PropTypes from 'prop-types';
-// import Big from 'big.js';
 import App from './App';
 
 ReactDOM.render(
